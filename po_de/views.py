0a4323908c3b346fac26ad7d067c981f4e2a7842
kavitha <kavinu2010@gmail.com>
2024-12-11 11:54:34 +0100

    created models and functions in views

--- po_de/views.py
+++ po_de/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def home(request):
+   postii=Post.objects.all()
+   return request(render, 'home.html',{'postii':postii})
+
+# def post(request):
+#   if request.method=='POST':
