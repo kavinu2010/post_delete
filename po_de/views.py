7ef3b59d18b3506720c73e5ca1163182cc2afa99
kavitha <kavinu2010@gmail.com>
2024-12-11 12:46:42 +0100

    post html added

--- po_de/views.py
+++ po_de/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def home(request):
    postii=Post.objects.all()
    return render(request, 'home.html',{'postii':postii})
-
-# def post(request):
-#   if request.method=='POST':
+ 
+def posts(request):
+   if request.method('POST'):
+      postts=PostForm(request.POST)
+      if postts.is_valid():
+         postts.save()
+         return redirect('home')
+      
+   else :
+      postts=PostForm()
+   return render(request, 'post.html',{'postts':postts})
